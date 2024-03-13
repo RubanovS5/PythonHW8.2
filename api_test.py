9f7d79a8d85d64132f723182db3c8407acd6de16
Александр Рубанов <price2016@gmail.com>
2024-03-13 12:50:50 +0300

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -4,15 +4,18 @@ from employeApi import EmployeeApi
 url = "https://x-clients-be.onrender.com"
 
 api = EmployeeApi(url)
+
 def test_create_new_employee():
     user = 'bloom'
     password = 'fire-fairy'
     api.get_token(user, password)  
-    name = "Новая"
-    description = "Супер 555"
-    company_id = api.create_new_company(name=name, description=description)
-    employee_before = api.get_employee(name=name, description=description)  
+    name = "Призрак"
+    description = "Дзин"
+    company_id = api.create_new_company(name, description)
+    employee_before = api.get_employee(name, description)
     api.add_employee(
+        name = name,
+        description = description,
         id="3158",
         first_name="Sasha",
         last_name="Doe",
@@ -24,13 +27,5 @@ def test_create_new_employee():
         birthdate='2024-03-06T11:24:56.028Z',
         is_active=True
     )
-    employee_after = api.get_employee(name=name, description=description)
+    employee_after = api.get_employee(name, description)
     assert  len(employee_before) < len(employee_after) 
-
-def test_create_new_employee_76421():
-    user = 'bloom'
-    password = 'fire-fairy'
-    api.get_token(user, password)  
-    name = "Топовая"
-    description = "Лучшая компания"
-    company_id = api.create_new_company(name = name, description=description)
