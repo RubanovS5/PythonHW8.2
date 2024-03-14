b7ade6e6c43cffcd4dbc4dd2d5d57b1b26efa5e0
Александр Рубанов <price2016@gmail.com>
2024-03-14 18:41:59 +0300

    Update employeApi.py

--- employeApi.py
+++ employeApi.py
@@ -20,39 +20,50 @@ class EmployeeApi:
         return resp.json()["id"]
     
 
-    def get_employee(self, name, description):
-        params = {
-        'name': name,
-        'description': description
-    }
-        my_headers = {}
-        my_headers["x-client-token"] = self.get_token()
-        resp = requests.get(self.url + '/employee', params=params, headers=my_headers)
-        return resp.json()
+    def get_employee(self,company_id):
+        params = {'company': company_id}
+        response = requests.get(self.url + '/employee', params=params)
+        return response.json()
 
-
-    def add_employee(self, name, description, id, first_name, last_name, middle_name, company_id, mail, employee_url, phone, birthdate, is_active):
-        company = {"name": name, 
-        "description": description}
-        my_headers = {}
-        my_headers["x-client-token"] = self.get_token()
-        resp = requests.post(self.url + '/company', json=company, headers=my_headers)
-        company_id = resp.json()["id"]
+    def add_employee(self, id, company_id, first_name, last_name, middle_name, mail, employee_url, phone, birthdate, is_active):
         employee_data = {
         "id": id,
-        "first_name": first_name,
-        "last_name": last_name,
-        "middle_name": middle_name,
-        "company_id": company_id,
-        "mail": mail,
-        "employee_url": employee_url,
+        "firstName": first_name,
+        "lastName": last_name,
+        "middleName": middle_name,
+        "companyId": company_id,
+        "email": mail,
+        "url": employee_url,
         "phone": phone,
         "birthdate": birthdate,
-        "is_active": is_active
+        "isActive": is_active
     }
-        if not all([first_name, last_name, company_id, mail, phone, birthdate]): 
-          raise ValueError("Не все обязательные поля заполнены") 
+        if company_id is None:
+            company_id = self.create_new_company()
+        if not all([first_name, last_name, company_id, mail, phone, birthdate]):
+            raise ValueError("Не все обязательные поля заполнены")
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
-        resp = requests.post(self.url + '/employee', json = employee_data, headers = my_headers)
+        resp = requests.post(self.url + '/employee', json=employee_data, headers=my_headers)
         return resp.json()
+    
+    def get_employee_id(self, id_get):
+       resp = requests.get(self.url+f"/employee/{id_get.get('id')}")
+       return resp.json()
+    
+    
+    def change_employee(self, id, change_lastName, change_email, change_url, change_phone, change_active):
+        change_employee = {
+        "lastName": change_lastName,
+        "email": change_email,
+        "url": change_url,
+        "phone": change_phone,
+        "isActive": change_active
+    }
+        my_headers = {}
+        my_headers["x-client-token"] = self.get_token()
+        resp = requests.patch(self.url+f"/employee/{id.get('id')}", json=change_employee, headers=my_headers)
+        return resp.json()
+    
+
+    
